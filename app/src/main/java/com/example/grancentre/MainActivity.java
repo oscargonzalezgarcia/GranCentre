85c211ea36ee9ee8758b29db7638689613acad5f
oscargonzalezgarcia <oscar.goga10@gmail.com>
2020-11-09 09:06:44 +0100

    Hacer que cada boton este escuchando el layout(OnClickListener)

--- app/src/main/java/com/example/grancentre/MainActivity.java
+++ app/src/main/java/com/example/grancentre/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -11,10 +12,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnNgci);
+        btn.setOnClickListener(this);
+
+        btn = findViewById(R.id.btnRest);
+        btn.setOnClickListener(this);
+
+        btn = findViewById(R.id.btnPrkng;
+        btn.setOnClickListener(this);
+
+        btn = findViewById(R.id.btnMvis);
+        btn.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
+
+        if(v.getId()==R.id.btnNgci)
+        {
+
+        }
+        else if(v.getId()==R.id.btnRest)
+        {
+
+        }
+        else if(v.getId()==R.id.btnPrkng)
+        {
+
+        }
+        else if(v.getId()==R.id.btnMvis)
+        {
+
+        }
 
     }
 }
