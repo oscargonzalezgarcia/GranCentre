c0068753ec462d1dcc4b194634b8da10aefb4d92
EduardMartinezChale <01martinezeduard@gmail.com>
2020-11-09 08:38:46 +0100

    Creacion: Activity Restaurant,Movies,Parkings,Business

---
+++ app/src/main/java/com/example/grancentre/Activity_Parkings.java
@@ -0,0 +1,14 @@
+package com.example.grancentre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Parkings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__parkings);
+    }
+}
