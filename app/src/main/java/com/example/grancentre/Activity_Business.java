02242bae1fce03e0c60440385b2db0ad6f4da278
oscargonzalezgarcia <oscar.goga10@gmail.com>
2020-11-16 09:50:52 +0100

    TABLAYOUT FUNCIONAL

--- app/src/main/java/com/example/grancentre/Activity_Business.java
+++ app/src/main/java/com/example/grancentre/Activity_Business.java
@@ -1,14 +1,41 @@
 package com.example.grancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
 public class Activity_Business extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private  ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__business);
+
+        setUpView();
+        setUpViewPagerAdapter();
     }
+
+    private void setUpViewPagerAdapter() {
+        viewPagerAdapter.addFragment(new PerruqueriaFragment(), "Perruqueries");
+        viewPagerAdapter.addFragment(new PerruqueriaFragment(), "Tallers");
+        viewPagerAdapter.addFragment(new PerruqueriaFragment(), "Botigues");
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setUpView() {
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+    }
+
 }
