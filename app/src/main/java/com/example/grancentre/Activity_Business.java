a685e518420f6c9dca8a3f3af5e23db62f5ce099
Eva <egutierrez@educem.com>
2020-11-25 09:49:11 +0100

    Afegir Interface al fragment i mètode onAttach per comunicar ActivityBusiness i PerruqueriaFragment

--- app/src/main/java/com/example/grancentre/Activity_Business.java
+++ app/src/main/java/com/example/grancentre/Activity_Business.java
@@ -3,12 +3,15 @@ package com.example.grancentre;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.tabs.TabLayout;
 
-public class Activity_Business extends AppCompatActivity {
+public class Activity_Business extends AppCompatActivity implements PerruqueriaFragment.OnClickPerruqueriaListener {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -38,4 +41,23 @@ public class Activity_Business extends AppCompatActivity {
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
     }
 
+    @Override
+    public void onClickPerruqueria(View v) {
+        if(v.getId()==R.id.iv1)
+        {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://bellesaessencial.com/"));
+            startActivity(intent);
+        }
+        else if(v.getId()==R.id.iv2){
+            //truquem per telefon
+            Intent intent2 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:938708807"));
+            startActivity(intent2);}
+        else
+        {
+            Uri gmmIntentUri = Uri.parse("geo:41.609627, 2.287949");
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            startActivity(mapIntent);
+        }
+    }
 }
